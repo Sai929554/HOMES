82808a2b12a7b300e9660bb46191822af6d9c21b
Sai929554 <shivarathrisaikumar991@gmail.com>
2025-03-27 10:09:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -507,6 +507,7 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(debug=True)
